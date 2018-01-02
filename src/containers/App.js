0545e48c415a627334d73772ad58791b01503a09
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-02 14:22:00 -0600

    Added medications stub array to see medications on list/passed it as props

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Medications from './Medications';
 
+const medications = [
+	{
+		name: "Zestril",
+		generic_name: "Lisinopril",
+		uses: "Lisinopril is used to treat high blood pressure",
+		side_effects: "dizziness, lightheadedness, tiredness, headaches",
+		precautions: "This drug may make you dizzy. Do not drive, use machinery, or do any activity that requires alertness until you are sure you can perform such activities safely."
+	},
+	{
+		name: "Zyrtec",
+		generic_name: "Cetirizine",
+		uses: "Cetirizine is an antihistamine used to relieve allergy symptoms such as watery eyes, runny nose, itching eyes/nose, sneezing, hives, and itching",
+		side_effects: "Drowsiness, tiredness, and dry mouth may occur. Stomach pain may also occur, especially in children",
+		precautions: "This drug may make you drowsy. Alcohol or marijuana can make you more drowsy. Do not drive, use machinery, or do anything that needs alertness until you can do it safely."
+	}
+]
+
 class App extends Component {
   render() {
     return (
@@ -9,7 +26,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to the Medications App!</h1>
         </header>
-        <Medications />
+        <Medications medications={medications} />
       </div>
     );
   }
--- src/containers/Medications.js
+++ src/containers/Medications.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const Medications = () => (
+const Medications = (props) => (
 	<div>
-		<h3>Medication Component</h3>
+		<h3>Medications</h3>
+			{props.medications.map(medication => 
+				<div>
+					<h4>Name: {medication.name}</h4>
+					<p>Generic Name: {medication.generic_name}</p>
+				</div>
+			)}	
 	</div>
-);
+)
 
 export default Medications;
 
