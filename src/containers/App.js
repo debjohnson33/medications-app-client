a0ad827048ae61462a3828b8a29e4a0cad9fc038
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-04 15:38:35 -0600

    Imported Home into App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Medications from './Medications'
+import Home from '../components/Home'
 import './App.css';
 
 const medications = [
