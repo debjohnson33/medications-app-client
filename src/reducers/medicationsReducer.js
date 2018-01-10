42ec725863124ba5f962c97bf60d261cb02556d7
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-10 17:30:17 -0600

    Edited reducer/action to add loading_medications type

--- src/reducers/medicationsReducer.js
+++ src/reducers/medicationsReducer.js
@@ -1,5 +1,9 @@
-export default (state = [], action) => {
+export default (state = {
+	loading: false, medications: []
+}, action) => {
 	switch (action.type) {
+		case 'LOADING_MEDICATIONS':
+			return Object.assign({}, state, { loading: true })
 		case 'FETCH_MEDICATIONS':
 			return action.medications;
 		case 'ADD_MEDICATION':
