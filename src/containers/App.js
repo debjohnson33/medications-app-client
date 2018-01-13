156ed23e0a9ebbabd0d0c6a6d580bd6f606598f6
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-13 15:13:18 -0600

    Added individual medication route to Routes in App/edited Medication component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import MedicationsPage from './MedicationsPage';
+import Medication from '../components/Medication';
 import Home from '../components/Home';
 import Navbar from '../components/Navbar';
 import './App.css';
+
 
 class App extends Component {
   render() {
@@ -14,6 +17,7 @@ class App extends Component {
 	      	<Switch>
 	      		<Route exact path='/' component={Home}/>
 	      		<Route exact path='/medications' component={MedicationsPage}/>
+	      		<Route exact path='/medications/:id' component={Medication}/>
 	      	</Switch>
 	      </div>
 	    </Router>
