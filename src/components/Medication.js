b516497b1a6cf2f5c189a68300b584a93ab42b52
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-13 18:31:39 -0600

    Took centering out of css/changed link to not include label/changed to call medicationShow in Medication component

--- src/components/Medication.js
+++ src/components/Medication.js
@@ -13,18 +13,27 @@ class Medication extends Component {
 	}
 
 	render() {
+		const medicationShow = () => {
+			const medications = this.props.medications;
+			const medicationId = parseInt(this.props.match.params.id);
+			const filteredMedication = medications.filter(medication => medication.id === medicationId);
+				return filteredMedication.map(medication => {
+					return(
+						<div key={medication.id}>
+							<p>Name: {medication.name}</p>
+							<p>Generic Name: {medication.generic_name}</p>
+							<p>Uses: {medication.uses}</p>
+							<p>Side Effects: {medication.uses}</p>
+							<p>Precautions: {medication.precautions}</p>
+						</div>
+					)
+				})
+		}
 
-		const medications = this.props.medications;
-		const medicationId = parseInt(this.props.match.params.id);
-		const filteredMedication = medications.filter(medication => medication.id === medicationId);
-		const medication = filteredMedication[0];
 		return (
 			<div>
-				<p>Name: {medication.name}</p>
-				<p>Generic Name: {medication.generic_name}</p>
-				<p>Uses: {medication.uses}</p>
-				<p>Side Effects: {medication.uses}</p>
-				<p>Precautions: {medication.precautions}</p>
+				<h3>Medication</h3>
+				{medicationShow()}
 			</div>
 		)
 	}
