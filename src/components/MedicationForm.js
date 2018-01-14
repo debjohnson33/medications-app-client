ec8da863c25fee9dd0d987f0de4e7cdc6d8014a8
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-13 21:19:57 -0600

    Added handleOnChange/handleOnSubmit & edited import

--- src/components/MedicationForm.js
+++ src/components/MedicationForm.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { updateMedicationFormData } from '../../actions/medicationForm';
-import { createMedication } from '../../actions/medicationForm';
+import { updateMedicationFormData } from '../actions/medicationForm';
+import { createMedication } from '../actions/medications';
 
 class MedicationForm extends Component {
 
+	handleOnChange = event => {
+		const { name, value } = event.target;
+		const currentMedicationFormData = Object.assign({}, this.props.medicationFormData, {
+			[name]: value
+		})
+		this.props.updateMedicationFormData(currentMedicationFormData)
+	}
+
+	handleOnSubmit = event => {
+		event.preventDefault()
+		this.props.createMedication(this.props.medicationFormData)
+	}
+
 	render() {
+
+		const { name, generic_name, uses, side_effects, precautions } = this.props.medicationFormData;
 		return (
 			<div>
 			<h4>Add a Medication to the List</h4>
@@ -35,10 +50,17 @@ class MedicationForm extends Component {
 					<label htmlFor='precautions'>Precautions:</label>
 					<input type='text' precautions='precautions' value={precautions} />
 				</div>
+				<button type="submit">Add Medication</button>
 			</form>
 			</div>
 		)
 	}
 }
 
-export default MedicationForm;
+const mapStateToProps = state => {
+	return {
+		medicationFormData: state.medicationFormData
+	}
+}
+ 
+export default connect(mapStateToProps, {updateMedicationFormData, createMedication})(MedicationForm);
