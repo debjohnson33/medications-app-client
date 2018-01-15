3a2c9bc878b413ad39e7d5c008e20fb14bc6a10d
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-15 16:24:51 -0600

    Added reset medication form to switch

--- src/reducers/medicationFormData.js
+++ src/reducers/medicationFormData.js
@@ -10,6 +10,8 @@ export default (state = initialState, action) => {
 	switch(action.type) {
 		case 'UPDATED_DATA':
 			return action.medicationFormData
+		case 'RESET_MEDICATION_FORM':
+			return initialState;
 		default:
 			return state;
 	}
