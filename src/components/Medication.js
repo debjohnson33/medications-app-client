91a9347b271b370d4a4fd8863178e83d2db6c5f3
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-16 13:41:09 -0600

    Added delete button to Medication component

--- src/components/Medication.js
+++ src/components/Medication.js
@@ -27,7 +27,7 @@ class Medication extends Component {
 							<p>Uses: {medication.uses}</p>
 							<p>Side Effects: {medication.uses}</p>
 							<p>Precautions: {medication.precautions}</p>
-							<Link to={`/medications/${medication.id}/edit`}>Edit Medication</Link>
+							<button>Delete Medication</button>
 							<h3>Reviews:</h3>
 							<Reviews reviews={medication.reviews}/>
 						</div>
