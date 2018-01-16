7d04a4c63fd3c1a5a495f822e659260ad0e27315
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-16 14:52:17 -0600

    Added createReview to reviews actions/started form in ReviewForm

--- src/actions/reviews.js
+++ src/actions/reviews.js
@@ -1,1 +1,28 @@
-reviews.js
+import { resetReviewForm } from './reviewForm';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const addReview = review => {
+	return {
+		type: 'CREATE_REVIEW_SUCCESS',
+		review
+	}
+}
+
+export const createReview = (review, reviewId) => {
+	return dispatch => {
+		return fetch(`${API_URL}/reviews`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ review: review })
+		})
+		.then(response => response.json())
+		.then(review => {
+			dispatch(addReview(review))
+			dispatch(resetReviewForm())
+		})
+		.catch(error => console.log(error))
+	}
+}
--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,1 +1,30 @@
-ReviewForm.js
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { updateReviewFormData } from '../actions/reviewForm';
+import { createReview } from '../actions/reviews';
+
+class ReviewForm extends Component {
+
+
+	render() {
+
+		return (
+			<div>
+				<form>
+					
+				</form>
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		reviewFormData: state.reviewFormData
+	}
+}
+ 
+export default connect(mapStateToProps, {
+	createReview
+})(ReviewForm);
