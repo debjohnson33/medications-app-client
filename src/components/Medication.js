84597143fa41294f7df31c09905f1abb39e8b571
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-19 20:27:04 -0600

    Added medication to ReviewForm props

--- src/components/Medication.js
+++ src/components/Medication.js
@@ -36,7 +36,7 @@ class Medication extends Component {
 							<p>Precautions: {medication.precautions}</p>
 							<button onClick={this.handleOnDelete}>Delete Medication</button><br /><br />
 							<p>Add a Review:</p>
-							<ReviewForm medication_id={medication.id}/>
+							<ReviewForm medication={medication} medication_id={medication.id}/>
 							<h3>Reviews:</h3>
 							<Reviews reviews={medication.reviews}/>
 						</div>
