56ee6d204e936b410999693a1d96d5ba6a6165bf
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-30 14:53:29 -0600

    Added columns to Medication component/edited Medication css

--- src/components/Medication.js
+++ src/components/Medication.js
@@ -6,6 +6,7 @@ import { fetchReviews } from '../actions/reviews';
 import Reviews from './Reviews';
 import ReviewForm from './ReviewForm';
 import './Medication.css';
+import '../containers/medicationsPage.css';
 
 class Medication extends Component {
 
@@ -30,17 +31,23 @@ class Medication extends Component {
 			const filteredMedication = medications.filter(medication => medication.id === medicationId);
 				return filteredMedication.map(medication => {
 					return(
-						<div key={medication.id}>
+						<div className="row">
+						<div key={medication.id} className="col-4">
 							<p>Name: {medication.name}</p>
 							<p>Generic Name: {medication.generic_name}</p>
 							<p>Uses: {medication.uses}</p>
 							<p>Side Effects: {medication.uses}</p>
 							<p>Precautions: {medication.precautions}</p>
 							<button className="med" onClick={this.handleOnDelete}>Delete Medication</button><br /><br />
+						</div>
+						<div className="col-4">
 							<p>Add a Review:</p>
 							<ReviewForm medication={medication} medication_id={medication.id}/>
+						</div>
+						<div className="col-4">
 							<h3>Reviews:</h3>
 							<Reviews medication_id={medicationId} medication={medication} />
+						</div>
 						</div>
 					)
 				})
