4b5f65076fd201a517b7b0bf2592ac6dd3d1145a
debjohnson33 <debjohnson_11@yahoo.com>
2018-02-10 18:30:08 -0600

    Edited footer/css for footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,18 +9,24 @@ const Footer = () => {
 			<footer>
 				<div className="row">
 					<div className="col-4">
-						<h3>About Medications App</h3>
-						<Link to={`/medications/about`}>About</Link>
+						<div className="box">
+							<h3>About Medications App</h3>
+							<Link to={`/medications/about`}>About</Link>
+						</div>
 					</div>
 					<div className="col-4">
-						<h3>External Links</h3>
-						<a target="_blank" href="https://www.medicinenet.com/medications/article.htm" rel="noopener noreferrer" alt="Medicine Net Medication Index">Medicine Net Medication Index</a><br />
-						<a target="_blank" href="https://www.rxlist.com/" rel="noopener noreferrer">RxList</a>	
+						<div className="box">
+							<h3>External Links</h3>
+							<a target="_blank" href="https://www.medicinenet.com/medications/article.htm" rel="noopener noreferrer" alt="Medicine Net Medication Index">Medicine Net Medication Index</a><br />
+							<a target="_blank" href="https://www.rxlist.com/" rel="noopener noreferrer">RxList</a>	
+						</div>
 					</div>
 					<div className="col-4">
-						<h3>Contact Us</h3>
-						<a href="mailto:debjohnson_11@yahoo.com">Send email to developer</a>
-						<p>On Stack Overflow: @debjohnson_11</p>						
+						<div className="box">
+							<h3>Contact Us</h3>
+							<a href="mailto:debjohnson_11@yahoo.com">Send email to developer</a>
+							<p>On Slack: @debjohnson_11</p>						
+						</div>
 					</div>
 				</div>
 				
