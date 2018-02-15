3e38d2977e4dfa5d9c06eb11b8d5acfc45d34fe9
debjohnson33 <debjohnson_11@yahoo.com>
2018-02-15 12:19:14 -0600

    Added GitHub url to About page

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = (props) => {
 	        <p id="appInfo">This app is intended as informative only and should not be used as medical advice.
 	         Talk to your doctor for any problems you may be having.</p>
 	        <p id="appInfo">This is an open sourced project, so if you would like to contribute to this project, please go to 
-	        my <a href="" target="_blank" rel="noopener noreferrer">Github Page</a></p>
+	        my <a href="https://github.com/debjohnson33/medications-app-api" target="_blank" rel="noopener noreferrer">Github Page</a></p>
 	        <p id="appInfo">Created by Deb Johnson as a project for Flatiron School</p>
 			<br />
 			<br />
