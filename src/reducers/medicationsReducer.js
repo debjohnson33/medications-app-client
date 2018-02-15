7b17bff79aaa1305f0dabfe4c50786d18a358d9b
debjohnson33 <debjohnson_11@yahoo.com>
2018-02-15 16:52:34 -0600

    Added ADD_LIKE to medications reducer

--- src/reducers/medicationsReducer.js
+++ src/reducers/medicationsReducer.js
@@ -6,6 +6,15 @@ export default (state = [], action) => {
 			return state.concat(action.medication)
 		case 'DELETE_MEDICATION_SUCCESS':
 			return state.filter(medication => medication.id !== action.medicationId);
+		case 'ADD_LIKE':
+			let index = state.findIndex(medication => medication.id === action.id);
+			let medication = state[index];
+	
+			return [
+				...state.slice(0, index),
+				Object.assign({}, medication, { likes: medication.likes += 1 }),
+				...state.slice(index + 1)
+			];
 		default:
 			return state;
 	}
